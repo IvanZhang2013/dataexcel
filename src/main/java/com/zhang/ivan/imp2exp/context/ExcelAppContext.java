f1c7324cc76e82753ba225a7faa5147f6d54321e
IvanZhang2013 <gongyishenhuxi2005@163.com>
2015-11-06 16:15:50 +0800

    修改  数据源统一使用BaseDataSOurce

--- src/main/java/com/zhang/ivan/imp2exp/context/ExcelAppContext.java
+++ src/main/java/com/zhang/ivan/imp2exp/context/ExcelAppContext.java
@@ -2,19 +2,17 @@ package com.zhang.ivan.imp2exp.context;
 
 import javax.sql.DataSource;
 
+import com.zhang.ivan.imp2exp.BaseDataConnection;
 import com.zhang.ivan.imp2exp.bean.BatchImportInfoVO;
 import com.zhang.ivan.imp2exp.bean.ExcelConfig;
 
 /**
  * 
- * 导入文件上下文，
- * 需要存储倒入文件的配置， 
- * 需要将数据源传递进去
- * 进行数据的提交修改和数据的数据库校验
+ * 导入文件上下文， 需要存储倒入文件的配置， 需要将数据源传递进去 进行数据的提交修改和数据的数据库校验
  */
 public class ExcelAppContext implements IExcelAppContext {
 
-	protected DataSource dataSource;
+	protected BaseDataConnection baseDataConnection;
 	protected BatchImportInfoVO batchImportInfoVO;
 	protected ExcelConfig excelConfig;
 
@@ -34,14 +32,6 @@ public class ExcelAppContext implements IExcelAppContext {
 		initExcelAppContext();
 	}
 
-	public DataSource getDataSource() {
-		return dataSource;
-	}
-
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
 	public ExcelAppContext initExcelAppContext() {
 		return null;
 	}
@@ -52,6 +42,14 @@ public class ExcelAppContext implements IExcelAppContext {
 
 	public void setExcelConfig(ExcelConfig excelConfig) {
 		this.excelConfig = excelConfig;
+	}
+
+	public BaseDataConnection getBaseDataConnection() {
+		return baseDataConnection;
+	}
+
+	public void setBaseDataConnection(BaseDataConnection baseDataConnection) {
+		this.baseDataConnection = baseDataConnection;
 	}
 
 }
