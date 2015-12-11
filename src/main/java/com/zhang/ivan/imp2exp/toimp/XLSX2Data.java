63fbbf3ab7af1496e8ed0de73f3e41767c19098b
IvanZhang2013 <gongyishenhuxi2005@163.com>
2015-12-11 15:24:57 +0800

    增加返回信息

--- src/main/java/com/zhang/ivan/imp2exp/toimp/XLSX2Data.java
+++ src/main/java/com/zhang/ivan/imp2exp/toimp/XLSX2Data.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import com.zhang.ivan.imp2exp.bean.ImpExcelTemplate;
 import com.zhang.ivan.imp2exp.check.bean.ExcelCheckContext;
+import com.zhang.ivan.imp2exp.check.bean.ExcelResult;
 import com.zhang.ivan.imp2exp.context.ExcelAppContext;
 
 /**
@@ -22,11 +23,11 @@ public class XLSX2Data extends AbstractExcel2Data implements ToImp {
 	public static Logger logger = LoggerFactory.getLogger(XLS2Data.class);
 
 	@Override
-	public void excute(ImpExcelTemplate imp, ExcelCheckContext excelCheckContext) throws Exception {
+	public ExcelResult excute(ImpExcelTemplate imp, ExcelCheckContext excelCheckContext) throws Exception {
 		String uploadFile = imp.getUploadFilePath();
 		xlsxWorkBook = new XSSFWorkbook(new FileInputStream(uploadFile));
-		JupiterService.jupiterLanceService(xlsxWorkBook, excelAppContext, excelCheckContext);
-
+		ExcelResult excelResult = JupiterService.jupiterLanceService(xlsxWorkBook, excelAppContext, excelCheckContext);
+		return excelResult;
 	}
 
 	public void setExcelAppContext(ExcelAppContext excelAppContext) {
