8288a426568b1a0bd11452558c2c0bdedc6b7cab
IvanZhang2013 <gongyishenhuxi2005@163.com>
2016-01-11 14:09:02 +0800

    增加行数

--- src/main/java/com/zhang/ivan/imp2exp/col/math/DataMath.java
+++ src/main/java/com/zhang/ivan/imp2exp/col/math/DataMath.java
@@ -2,5 +2,5 @@ package com.zhang.ivan.imp2exp.col.math;
 
 public interface DataMath {
 
-	public String math(Object[] objArray) throws Exception;
+	public  String math(Object[] objArray ,int rowIndex)throws Exception;
 }
